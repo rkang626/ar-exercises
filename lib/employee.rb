32b3617f9ae629910bc45f658d4a73ea47458e1a
Ryan Kang <rkang626@gmail.com>
2021-03-02 13:12:25 +0000

    stretch exercises

--- lib/employee.rb
+++ lib/employee.rb
@@ -5,4 +5,12 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
   validates_associated :store
+
+  after_create :give_employee_a_password
+
+  private
+    def give_employee_a_password
+      self.password = ("a".."z").to_a.shuffle[0,8].join
+      self.save
+    end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -5,9 +5,21 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :carries_apparel
 
+  before_destroy :check_for_employees
+
   def carries_apparel
     if mens_apparel != true && womens_apparel != true
       errors.add(:base, "Store must carry mens or womens apparel")
     end
   end
+
+  private
+
+    def check_for_employees
+      employees = Employee.where(store_id: self.id)
+      if employees.any?
+        errors.add(:base, "Can't destroy stores that have employees")
+        return false
+      end
+    end
 end
